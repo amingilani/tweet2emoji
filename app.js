4c57ecbf208052fe9a656aecb3460ffd96c39531
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-16 13:05:06 +0500

    Track specific tweets

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-
-
-var express = require('express'),
-  config = require('./config/config');
+var express = require('express');
+var config = require('./config/config');
+var log = require('./lib/logger');
+var trackTerm = require('./lib/trackTerm');
 
 var app = express();
 
+// setup the app
 require('./config/express')(app, config);
 
 app.listen(config.port, function () {
-  console.log('Express server listening on port ' + config.port);
+  console.info('Express server listening on port ' + config.port);
 });
 
+// track a term
+trackTerm('obama', function(err, tweet) {
+  if (err) log.warning(err);
+  if (tweet) log.debug("@" + tweet.user.screen_name + " says: " + tweet.text);
+});
