17ad8ae3e0895115e7829b4ebacb6fb6652be615
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-17 22:42:47 +0500

    Maintain a single twitter connection, regardless of number of sessions

--- app.js
+++ app.js
@@ -5,28 +5,33 @@ var io = require('socket.io')(http);
 var config = require('./config/config');
 var log = require('./lib/logger');
 var trackTerm = require('./lib/trackTerm');
+var EventEmitter = require("events").EventEmitter;
+var ee = new EventEmitter();
+
 
 // setup the app
 require('./config/express')(app, config);
 
-http.listen(config.port, function () {
+http.listen(config.port, function() {
   log.info('Express server listening on port ' + config.port);
 });
 
 // setup socket.io
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   log.debug('a user connected');
 
-  // track a term
-  trackTerm('tweet2emoji', function(err, tweet) {
-
-    // log errors
-    if (err) return log.error(err);
-
-    // emit tweets
+  // send out tweets when they happen
+  ee.on("twitter", function (tweet) {
       socket.emit('tweet', tweet);
-      log.debug("Delivered tweet", tweet);
+      log.debug("Sent out tweet", tweet);
   });
+});
 
-
+// track a term
+trackTerm('tweet2emoji', function(err, tweet) {
+  // log errors
+  if (err) return log.error(err);
+  // emit tweets
+  ee.emit('twitter', tweet);
+  log.debug("Heard a tweet", tweet);
 });
