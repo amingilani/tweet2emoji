f9a8a686b92f13debc07759092cf92c938f7cc51
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-17 07:09:02 +0500

    Log tweets to console in front-end

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var config = require('./config/config');
 var log = require('./lib/logger');
 var trackTerm = require('./lib/trackTerm');
 
-var app = express();
-
 // setup the app
 require('./config/express')(app, config);
 
-app.listen(config.port, function () {
+http.listen(config.port, function () {
   log.info('Express server listening on port ' + config.port);
 });
 
-// track a term
-trackTerm('obama', function(err, tweet) {
-  if (err) log.warning(err);
-  if (tweet) log.debug("@" + tweet.user.screen_name + " says: " + tweet.text);
+// setup socket.io
+io.on('connection', function(socket){
+  log.debug('a user connected');
+
+  // track a term
+  trackTerm('obama', function(err, tweet) {
+
+    // log errors
+    if (err) log.error(err);
+
+    // emit tweets
+    if (tweet) {
+      socket.emit('tweet', tweet);
+      log.debug("Delivered tweet", tweet);
+    }
+  });
+
+
 });
