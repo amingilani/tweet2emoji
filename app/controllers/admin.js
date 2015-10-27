d9f3fd68dd9464a06dcdf95246f441feb4b51b01
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-27 23:04:24 +0500

    Add dashboard route

---
+++ app/controllers/admin.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/dashboard', function (req, res, next) {
+    res.render('dasboard');
+});
