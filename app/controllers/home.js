b8c77f27966db5e7e2d74eff21287e86cacded6b
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-27 22:35:21 +0500

    Remove sample code

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,9 +7,5 @@ module.exports = function (app) {
 };
 
 router.get('/', function (req, res, next) {
-  var articles = [new Article(), new Article()];
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
+    res.render('index');
 });
