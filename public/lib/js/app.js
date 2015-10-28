4c46fde58b434f21e5e6d5be1c213ba711de590f
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-28 12:10:31 +0500

    Add display queue

--- public/lib/js/app.js
+++ public/lib/js/app.js
@@ -1,23 +1,35 @@
 var app = angular.module("myapp", []);
 
-app.controller('MyCtrl', function($scope) {
+app.controller('MyCtrl', ['$scope', '$interval', function($scope, $interval) {
   // the tweets for the admin panel
-  $scope.tweets = [
-  ];
+  $scope.tweets = [];
   $scope.addNewItem = function(newItem) {
     $scope.tweets.push(angular.copy(newItem));
     console.log("added item to tweets");
   };
 
   // the tweets for the display panel
-  $scope.display = [
-  ];
+  $scope.display = [];
+  $scope.currentDisplay = {};
   $scope.addToDisplay = function(newItem) {
     $scope.display.push(angular.copy(newItem));
     console.log("added item to display");
   };
 
-});
+  // set the refresh interval
+  $interval(function() {
+    var pointer = $scope.display
+      .shift();
+    if (pointer === undefined) {
+    $scope.currentDisplay.emoji = "Nothing to display";
+    console.log("Nothing to display");
+    } else {
+    $scope.currentDisplay = pointer;
+      console.log("Shifting from display to currentDisplay");
+  }
+}, 1 * 1000);
+
+}]);
 app.filter('reverse', function() {
   return function(tweets) {
     return tweets.slice().reverse();
