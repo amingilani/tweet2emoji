c792199b1ed95d62df0f6ffa44510e3806e2bc3d
Rana Waleed <waleedsulehria@gmail.com>
2015-10-29 10:06:21 +0500

    add screen name

--- public/lib/js/app.js
+++ public/lib/js/app.js
@@ -24,7 +24,8 @@ app.controller('MyCtrl', ['$scope', '$interval', function($scope, $interval) {
     $scope.currentDisplay.emoji = "";
     console.log("Nothing to display");
     } else {
-      pointer.emoji = emojione.unicodeToImage(pointer.emoji)
+      pointer.tweet = JSON.parse(pointer.tweet);
+      pointer.emoji = emojione.unicodeToImage(pointer.emoji)+" "+"<h1 style='margin-left='50px''>@" + pointer.tweet.user.screen_name + "</h1>";
       console.log(pointer.emoji+"<img src='lib\img\Pepsi-can.png' height='300px' style='margin-left:500px;margin-top:290px;position: absolute;clip: rect(100, 100px, 100px, 100px);'/>")
     $scope.currentDisplay = pointer;
       console.log("Shifting from display to currentDisplay");
